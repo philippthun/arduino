29c7111d6c4167574b3dcdcb6c0d0ca401bb4d51
Philipp Thun <philipp.thun@sap.com>
2013-09-02 11:29:24 +0200

    PedestrianLight - enhancements

--- PedestrianLight/PedestrianLight.ino
+++ PedestrianLight/PedestrianLight.ino
@@ -2,25 +2,26 @@
   Pedestrian light
  */
 
-const int carRedPin = 13;
-const int carYellowPin = 12;
-const int carGreenPin = 8;
-const int pedRedPin = 7;
-const int pedGreenPin = 4;
+const int carRedPin = 12;
+const int carYellowPin = 11;
+const int carGreenPin = 10;
+const int pedRedPin = 9;
+const int pedGreenPin = 8;
+const int pedWaitPin = 13;
 const int buttonPin = 2;
 
-int carRedState = 0;
-int carYellowState = 0;
-int carGreenState = 0;
-int pedRedState = 0;
-int pedGreenState = 0;
-
-unsigned long time = 0;
-int buttonState = 0;
+unsigned long time;
+int buttonState;
+int lightState = 0;
 boolean pedPushed = false;
-boolean pedHasGreen = false;
 unsigned long waitUntil = 0;
-int switchStep = 0;
+
+int carRedState = LOW;
+int carYellowState = LOW;
+int carGreenState = HIGH;
+int pedRedState = HIGH;
+int pedGreenState = LOW;
+int pedWaitState = LOW;
 
 void setup() {
   pinMode(carRedPin, OUTPUT);
@@ -28,73 +29,78 @@ void setup() {
   pinMode(carGreenPin, OUTPUT);
   pinMode(pedRedPin, OUTPUT);
   pinMode(pedGreenPin, OUTPUT);
+  pinMode(pedWaitPin, OUTPUT);
   pinMode(buttonPin, INPUT);
-
-  carRedState = LOW;
-  carYellowState = LOW;
-  carGreenState = HIGH;
-  pedRedState = HIGH;
-  pedGreenState = LOW;
 }
 
 void loop() {
   time = millis();
   buttonState = digitalRead(buttonPin);
 
-  if (buttonState == HIGH && pedPushed == false && pedHasGreen == false) {
+  if (buttonState == HIGH && (lightState == 0 || lightState >= 5)) {
     pedPushed = true;
-    if (waitUntil == 0 || waitUntil < time + 5000) {
-      waitUntil = time + 5000;
-    }
+    pedWaitState = HIGH;
   }
 
-  if (pedPushed == true && time > waitUntil) {
-    switch (switchStep) {
-    case 0:
+  switch (lightState) {
+  case 0:
+    if (pedPushed == true) {
+      pedPushed = false;
+      if (waitUntil == 0 || waitUntil < time + 5000) {
+        waitUntil = time + 5000;
+      }
+      lightState++;
+    }
+    break;
+  case 1:
+    if (time > waitUntil) {
       carYellowState = HIGH;
       carGreenState = LOW;
       waitUntil = time + 2000;
-      switchStep++;
-      break;
-    case 1:
+      lightState++;
+    }
+    break;
+  case 2:
+    if (time > waitUntil) {
       carRedState = HIGH;
       carYellowState = LOW;
       waitUntil = time + 2000;
-      switchStep++;
-      break;
-    case 2:
+      lightState++;
+    }
+    break;
+  case 3:
+    if (time > waitUntil) {
       pedRedState = LOW;
       pedGreenState = HIGH;
-      switchStep = 0;
-      pedPushed = false;
-      pedHasGreen = true;
+      pedWaitState = LOW;
       waitUntil = time + 10000;
-      break;
+      lightState++;
     }
-  }
-
-  if (pedHasGreen == true && time > waitUntil) {
-    switch (switchStep) {
-    case 0:
+    break;
+  case 4:
+    if (time > waitUntil) {
       pedRedState = HIGH;
       pedGreenState = LOW;
       waitUntil = time + 2000;
-      switchStep++;
-      break;
-    case 1:
+      lightState++;
+    }
+    break;
+  case 5:
+    if (time > waitUntil) {
       carYellowState = HIGH;
       waitUntil = time + 2000;
-      switchStep++;
-      break;
-    case 2:
+      lightState++;
+    }
+    break;
+  case 6:
+    if (time > waitUntil) {
       carRedState = LOW;
       carYellowState = LOW;
       carGreenState = HIGH;
-      switchStep = 0;
-      pedHasGreen = false;
       waitUntil = time + 20000;
-      break;
+      lightState = 0;
     }
+    break;
   }
 
   digitalWrite(carRedPin, carRedState);
@@ -102,5 +108,8 @@ void loop() {
   digitalWrite(carGreenPin, carGreenState);
   digitalWrite(pedRedPin, pedRedState);
   digitalWrite(pedGreenPin, pedGreenState);
+  digitalWrite(pedWaitPin, pedWaitState);
+
+  delay(50);
 }
 
