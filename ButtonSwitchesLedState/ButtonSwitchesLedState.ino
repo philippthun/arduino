4911d437a3f6e51cd3e2e0fcc764f779965ea513
Philipp Thun <philipp.thun@sap.com>
2013-09-02 11:23:34 +0200

    ButtonSwitchesLedState sketch

---
+++ ButtonSwitchesLedState/ButtonSwitchesLedState.ino
@@ -0,0 +1,38 @@
+/*
+  Button switches LED state
+ */
+
+const int buttonPin = 2;
+const int ledPin = 13;
+
+int buttonState = 0;
+int ledState = LOW;
+boolean changed = false;
+
+void setup() {
+  pinMode(buttonPin, INPUT);
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+    if (changed == false) {
+      if (ledState == HIGH) {
+        ledState = LOW;
+      } 
+      else {
+        ledState = HIGH;
+      }
+      digitalWrite(ledPin, ledState);
+      changed = true;
+    }
+  } 
+  else {
+    if (changed == true) {
+      changed = false;
+    }
+  }
+}
+
