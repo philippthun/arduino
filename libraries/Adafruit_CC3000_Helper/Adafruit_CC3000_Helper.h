b5c03d5d04e8af8efe2d873f9a5b3af113c35b78
Philipp Thun <philipp.thun@t-online.de>
2013-10-22 00:38:30 +0200

    Add license information

--- libraries/Adafruit_CC3000_Helper/Adafruit_CC3000_Helper.h
+++ libraries/Adafruit_CC3000_Helper/Adafruit_CC3000_Helper.h
@@ -1,9 +1,41 @@
 /*
   Adafruit CC3000 Helper
 
-  Reusable helper functions for the Adafruit CC3000 library
-  (https://github.com/adafruit/Adafruit_CC3000_Library). Most functions have been taken from or are
-  inspired by the library's examples.
+  Reusable helper functions for the Adafruit CC3000 library (https://github.com/
+  adafruit/Adafruit_CC3000_Library). Most functions have been taken from or are
+  inspired by the library's examples - see below for additional information.
+
+  --------------------------------------------------------------------------------
+  Copyright (c) 2013 Philipp Thun
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal in
+  the Software without restriction, including without limitation the rights to
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+  the Software, and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  --------------------------------------------------------------------------------
+  Additional copyright / license information:
+
+  Written by Adafruit Industries, distributed under BSD License.
+
+  Designed specifically to work with the Adafruit WiFi products:
+  ----> https://www.adafruit.com/products/1469
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+  --------------------------------------------------------------------------------
 */
 
 #ifndef Adafruit_CC3000_Helper_h
--- libraries/Robot/Robot.cpp
+++ libraries/Robot/Robot.cpp
@@ -1,8 +1,28 @@
 /*
-  Robot.cpp
+  Robot
+
+  --------------------------------------------------------------------------------
+  Copyright (c) 2013 Philipp Thun
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal in
+  the Software without restriction, including without limitation the rights to
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+  the Software, and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  --------------------------------------------------------------------------------
 */
 
-#include "Arduino.h"
 #include "Robot.h"
 
 const int PIN_RIGHT_BRAKE = 9;
@@ -13,8 +33,7 @@ const int PIN_LEFT_BRAKE = 8;
 const int PIN_LEFT_DIRECTION = 13;
 const int PIN_LEFT_SPEED = 11;
 
-Robot::Robot()
-{
+Robot::Robot() {
   pinMode(PIN_RIGHT_BRAKE, OUTPUT);
   pinMode(PIN_RIGHT_DIRECTION, OUTPUT);
   pinMode(PIN_RIGHT_SPEED, OUTPUT);
@@ -26,8 +45,7 @@ Robot::Robot()
   _brake();
 }
 
-void Robot::drive(boolean forward, int speed, int duration)
-{
+void Robot::drive(boolean forward, int speed, int duration) {
   digitalWrite(PIN_RIGHT_BRAKE, LOW);
   digitalWrite(PIN_LEFT_BRAKE, LOW);
 
@@ -48,8 +66,7 @@ void Robot::drive(boolean forward, int speed, int duration)
   _brake();
 }
 
-void Robot::rotate(boolean clockwise, int speed, int duration)
-{
+void Robot::rotate(boolean clockwise, int speed, int duration) {
   digitalWrite(PIN_RIGHT_BRAKE, LOW);
   digitalWrite(PIN_LEFT_BRAKE, LOW);
 
@@ -70,8 +87,7 @@ void Robot::rotate(boolean clockwise, int speed, int duration)
   _brake();
 }
 
-void Robot::_brake()
-{
+void Robot::_brake() {
   digitalWrite(PIN_RIGHT_BRAKE, HIGH);
   digitalWrite(PIN_LEFT_BRAKE, HIGH);
 
